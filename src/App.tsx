bb3440fddf7745e0a443b14e176f46063f882877
klebersilvars <kleber.600.l@gmail.com>
2025-11-22 21:01:51 -0300

    feat: speed insights da vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
 import AppRoutes from './appRoute'
 import { ChakraProvider } from '@chakra-ui/react'
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
   return (
     <ChakraProvider>
       <AppRoutes/>
+      <SpeedInsights />
+      
     </ChakraProvider>
   )
 }
